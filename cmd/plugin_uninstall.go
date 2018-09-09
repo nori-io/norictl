47d8fe7c746e8a855a3974143cca3d0c6e5d8553
Sergey <serge@cheebo.me>
2018-09-09 07:45:43 +0500

    uninstall command

---
+++ cmd/plugin_uninstall.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/net/context"
+
+	"github.com/secure2work/nori/proto"
+	"github.com/secure2work/norictl/client"
+	"github.com/fzzy/radix/redis/resp"
+)
+
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "install plugin",
+	Run: func(cmd *cobra.Command, args []string) {
+		id := viper.GetString("plugin-id")
+		if len(id) == 0 && len(args) > 0 {
+			id = args[0]
+		}
+
+		cli, closeCh := client.NewClient(
+			viper.GetString("grpc-address"),
+			viper.GetString("ca"),
+			viper.GetString("ServerHostOverride"),
+		)
+
+		reply, err := cli.UninstallCommand(context.Background(), &commands.UninstallRequest{Id: id})
+		defer close(closeCh)
+		if err != nil {
+			if reply != nil {
+				logrus.Fatal(reply.Error)
+			}
+			logrus.Fatal(err)
+		}
+
+		fmt.Printf("Plugin %s uninstalled, %3d :\n", id, resp.Int)
+	},
+}
+
+func init() {
+	pluginCmd.AddCommand(uninstallCmd)
+}
+
