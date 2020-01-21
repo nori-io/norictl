a5873c687f1a097eef5ccee18273cf9cc181b592
bruteforce1414 <bruteforce1414@gmail.com>
2020-01-21 14:21:19 +0500

    fix: add example with table

--- internal/ui/example/ui_example.go
+++ internal/ui/example/ui_example.go
@@ -12,6 +12,10 @@ func main(){
 	uiExample.GetFailure("nori/session:0.0.1")
 	uiExample.InstallSuccess("nori/session:0.0.2")
 	uiExample.InstallFailure("nori/session:0.0.3")
+	plugins:=make([][]string,2)
+	plugins=append(plugins, []string{"nori/session:0.0.4", "author1"})
+	plugins=append(plugins, []string{"nori/session:0.0.5", "author2"})
+	uiExample.PluginsAll(plugins)
 
 	fmt.Println()
 
